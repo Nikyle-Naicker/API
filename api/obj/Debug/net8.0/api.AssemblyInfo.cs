8d2e3af8239dd73c7c9190354feaa1099afef1a6
Nikyle-Naicker <nikylen@gmail.com>
2024-06-30 21:55:43 +0200

    Add a Dto to filter out certain information when needed

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bfd97a42d04239f27dce4abb565541301ac32f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f390a4a51553987d0c1a31a2b17dda7654eb90ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
