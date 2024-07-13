abc3450e46f257c2dea698cb208254ec06cb7b35
Nikyle-Naicker <nikylen@gmail.com>
2024-07-13 19:26:33 +0200

    Added create portfolio API endpoint and the delete portfolio endpoint

--- api/Controllers/PortfolioController.cs
+++ api/Controllers/PortfolioController.cs
@@ -9,6 +9,7 @@ using api.Repository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace api.Controllers
 {
@@ -37,5 +38,70 @@ namespace api.Controllers
             return Ok(userPortfolio);
         }
 
-    }
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> AddPortfolio(string symbol)
+        {
+            var username = User.GetUsername();
+            var AppUser = await _userManager.FindByNameAsync(username);
+
+            var stock = await _stockRepo.GetBySymbolAsync(symbol);
+
+            if(stock == null)
+            {
+                return BadRequest("Stock not found");
+            }
+
+            var portfolio = await _portfolioRepo.GetUserPortfolio(AppUser);
+
+            if(portfolio.Any(p => p.Symbol.ToLower() == symbol.ToLower()))
+            {
+                return BadRequest("Stock already added to portfolio");
+            }
+
+            var portfolioModel = new Portfolio
+            {
+                StockId = stock.Id,
+                AppUserId = AppUser.Id,
+            };
+
+            await _portfolioRepo.CreatePortfolio(portfolioModel);
+
+            if(portfolioModel == null)
+            {
+                return StatusCode(500, "Could not create portfolio");
+            }
+
+            return Created();            
+
+        }
+
+        [HttpDelete]
+        [Authorize]
+        public async Task<IActionResult> DeletePortfolio(string symbol)
+        {
+            var username = User.GetUsername();
+            var AppUser = await _userManager.FindByNameAsync(username);
+
+            var userPortfolio = await _portfolioRepo.GetUserPortfolio(AppUser);
+
+            var filteredstock = userPortfolio.Where(f => f.Symbol.ToLower() == symbol.ToLower());
+
+            if(filteredstock.Count() > 0)
+            {
+                await _portfolioRepo.DeletePortfolio(AppUser, symbol);
+                
+            }
+            else
+            {
+                return BadRequest("Stock not in your portfolio");
+            }
+            
+            return Ok("Portfolio deleted Successfully");
+            
+        
+        }
+
+    }  
+
 }
--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -75,6 +75,11 @@ namespace api.Repository
             return stockModel;
         }
 
+        public async Task<Stock?> GetBySymbolAsync(string symbol)
+        {
+            return await _context.Stock.FirstOrDefaultAsync(s => s.Symbol == symbol);
+        }
+
         public Task<bool> StockExists(int id)
         {
             return _context.Stock.AnyAsync(s => s.Id == id);
