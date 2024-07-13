48d34775b25e589d272dd2008f69532ec001feae
Nikyle-Naicker <nikylen@gmail.com>
2024-07-13 23:49:03 +0200

    Installed react and created new interface for the API

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f999e5dbf89a9d2703db4ba3f7836ee3801bf92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+867f709da097d5d4afedf20b0b37e1b10745f9b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
