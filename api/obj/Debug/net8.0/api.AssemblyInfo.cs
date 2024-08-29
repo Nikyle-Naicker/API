fe24f284c1e2e8c4bf380976720cf2e4702ef9d7
Nikyle-Naicker <nikylen@gmail.com>
2024-08-29 17:06:59 +0200

    Added the feature that allows users to add stocks to their portfolio

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+353b92b9b95c8db8c0ffb3903b7dc3dc4a805319")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a2cd727f5351691218602f47178901d752cb9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
