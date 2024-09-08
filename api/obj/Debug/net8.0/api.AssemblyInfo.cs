3a8a01cb133043f49ccf8008736faf4d761b5629
Nikyle-Naicker <nikylen@gmail.com>
2024-09-08 10:05:56 +0200

    Users can view the 10K annual report of stocks

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f88bb06f364d019e72c709b51b29b10c7059ad1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b71f00dc89cd6f2c2491cee1396d407d509348a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
