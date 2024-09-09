38a18b3438529ddcc6e49e9202dac43b6db9a9a9
Nikyle-Naicker <nikylen@gmail.com>
2024-09-09 13:56:49 +0200

    Added number formatting to the company page's components to make reading large numbers easier

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b71f00dc89cd6f2c2491cee1396d407d509348a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd44fb4682b5c239ac5f269e82aa5db26288dc4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- front-end/front-end/src/Pages/CompanyPage/CompanyPage.tsx
+++ front-end/front-end/src/Pages/CompanyPage/CompanyPage.tsx
@@ -34,7 +34,7 @@ const CompanyPage = (props: Props) => {
         <Sidebar/>
         <CompanyDashboard ticker={ticker!} >
           <Tile title="Company Name" subTitle={company.companyName}></Tile>
-          <Tile title="Price" subTitle={company.price.toString()}></Tile>
+          <Tile title="Price" subTitle={"$ " + company.price.toString()}></Tile>
           <Tile title="Currency" subTitle={company.currency}></Tile>
           <Tile title="Sector" subTitle={company.sector}></Tile>
           {/* <p className='bg-white shadow rounded text-medium text-gray-900 p-3 mt-1 m-4' >
