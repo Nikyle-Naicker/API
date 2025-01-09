26f1bb4560d0ba20881d9fbb75d1c0c39b36d169
Nikyle-Naicker <nikylen@gmail.com>
2025-01-09 11:58:14 +0200

    Removed unused imports and changed typescript version

--- front-end/front-end/src/Pages/DesignPage/DesignPage.tsx
+++ front-end/front-end/src/Pages/DesignPage/DesignPage.tsx
@@ -3,13 +3,7 @@
 
 type Props = {};
 
-const tableConfig = [
-    {
-      label: "Market Cap",
-      render: (company: any) => company.marketCapTTM,
-      subTitle: "Total value of all a company's shares of stock",
-    }
-]
+
 
 const DesignPage = (props: Props) => {
   return (
