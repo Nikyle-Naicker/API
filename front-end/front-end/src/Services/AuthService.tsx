308dae420f70197db7de86e4cacf5c437e39ae29
Nikyle Naicker <nikylen@gmail.com>
2025-01-29 12:12:58 +0200

    Update AuthService.tsx

--- front-end/front-end/src/Services/AuthService.tsx
+++ front-end/front-end/src/Services/AuthService.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { handleError } from "../Helpers/ErrorHandler";
 import { UserProfileToken } from "../Models/User";
 
-const api = "https://delightful-island-0a0d63a1e.4.azurestaticapps.net/";
+const api = "https://delightful-island-0a0d63a1e.4.azurestaticapps.net";
 
 export const loginAPI = async (username: string, password: string) => {
     try
@@ -25,7 +25,7 @@ export const loginAPI = async (username: string, password: string) => {
 export const registerAPI = async (email: string, username: string, password: string) => {
     try
     {
-        const data = await axios.post<UserProfileToken>("/api/account/register", {
+        const data = await axios.post<UserProfileToken>(api + "/account/register", {
             email: email,
             username: username,
             password: password
